deeef220c30625222001dafe679872072fe2bc18
noemie-graignac <noemie.graignac@edu.esiee.fr>
2024-11-08 13:26:29 +0000

    test de nb premier

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
-from math import sqrt
+""" Ce fichier sert à afficher le nombre s'il est premier"""
+# from math import sqrt
 
 #### Fonction secondaire
-
-
 def isprime(p):
+    """On cherche à savoir si le nombre est premier"""
 
+    if p < 2:
+        return False
+
+    for d in range(2,p):
+        if p%d==0:
+            return False
     # votre code ici
 
-    pass
+    return True
+
 
 #### Fonction principale
 
-
 def main():
+    """on affiche le nombre premier n"""
 
     # vos appels à la fonction secondaire ici
+    isprime(14)
+    isprime(3)
 
     for n in range(100):
         if isprime(n):
